aeec9c0e03def4972c9ca4023f9cf53b13c0bed8
firerdong <firerdong@qq.com>
2015-07-31 21:18:08 +0800

    fix mulit click

--- app/src/main/java/com/firer/changebackgroundcolor/lib/BgColorGradualLayout.java
+++ app/src/main/java/com/firer/changebackgroundcolor/lib/BgColorGradualLayout.java
@@ -3,15 +3,19 @@ package com.firer.changebackgroundcolor.lib;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.RelativeLayout;
 
 import com.firer.changebackgroundcolor.sample.R;
 
 public class BgColorGradualLayout extends RelativeLayout {
+
     private int baseColor;
     private int targetColor;
+
     private Handler canvasHandler;
     private Paint paint;
     private int animationDuration = 500;
@@ -47,13 +51,17 @@ public class BgColorGradualLayout extends RelativeLayout {
         }
     };
 
-    public void changeColor(int baseRgbColor, int targetRgbColor) {
+    public void changeColor() {
         if( !animationRunning ) {
-            baseColor = baseRgbColor;
-            targetColor = targetRgbColor;
-            animationRunning = true;
-            animationStage = 0;
-            invalidate();
+            ColorDrawable dr = (ColorDrawable)getBackground();
+            int color = dr.getColor();
+            if( color == baseColor || color == targetColor ){
+                animationRunning = true;
+                animationStage = 0;
+                invalidate();
+            } else {
+                //正在疯狂点击
+            }
         }
     }
 
@@ -63,11 +71,26 @@ public class BgColorGradualLayout extends RelativeLayout {
         if (animationRunning) {
             if (animationStage > animationDuration) {
                 animationRunning = false;
+                int temp = baseColor;
+                baseColor = targetColor;
+                targetColor = temp;
                 return;
             }
             setBackgroundColor(ColorUtils.getGradualColor(baseColor, targetColor, animationStage, animationDuration));
             animationStage += 10;
             canvasHandler.postDelayed(runnable, frameRate);
         }
+    }
+
+
+    public void setCurrentState(int baseColor,int targetColor){
+        this.baseColor = baseColor;
+        this.targetColor = targetColor;
+        setBackgroundColor(baseColor);
+
+    }
+
+    public int getCurrentState(){
+        return baseColor;
     }
 }
--- app/src/main/java/com/firer/changebackgroundcolor/sample/MainActivity.java
+++ app/src/main/java/com/firer/changebackgroundcolor/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.firer.changebackgroundcolor.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.firer.changebackgroundcolor.lib.BgColorGradualLayout;
@@ -19,16 +20,11 @@ public class MainActivity extends Activity {
         layout = (BgColorGradualLayout)findViewById(R.id.layout);
         startColor = getResources().getColor(R.color.start_color);
         endColor = getResources().getColor(R.color.end_color);
+        layout.setCurrentState(startColor,endColor);
     }
 
     public void click_change_color(View v){
-        if( state ){
-            layout.changeColor(startColor,endColor);
-            state = false;
-        } else {
-            layout.changeColor(endColor,startColor);
-            state = true;
-        }
+        layout.changeColor();
     }
 
 }
